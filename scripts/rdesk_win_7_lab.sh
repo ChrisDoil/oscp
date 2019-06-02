254852005521a698fdf9ed0f8b1efa22537de2a0
dante_user_1 <dante@localhost.localdomain>
2019-06-02 03:38:01 -0400

    20190602

---
+++ scripts/rdesk_win_7_lab.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "[+] Initializing Win 7 Lab Connection [+]"
+echo "[+] Remoting to IP - 10.11.23.107 [+]"
+
+rdesktop -u offsec -p jr3XWBFCA2O 10.11.23.107
--- scripts/startvpn.sh
+++ scripts/startvpn.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+set-title OS-41699 VPN
 echo -n jr3XWBFCA2O | xclip -selection c
 openvpn /home/dante/Desktop/OSCP/OS-41699-PWK.ovpn
